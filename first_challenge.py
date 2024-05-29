989405054655981f486db3b3cde47e2d75551c97
John Idowu <johntoby007@gmail.com>
2024-05-29 06:32:35 +0100

    Update first_challenge.py

--- first_challenge.py
+++ first_challenge.py
@@ -4,6 +4,7 @@ x = random.randint(1, 10)
 y = random.randint(1, 10)
 #prints various maths operations
 print(x*y)
+print (x+y)
 print("The sum of {}, {}, is {}".format(x, y, (x+y)))
 print("The product of {}, {}, is {}".format(x, y, x*y))
 print("{}, divided by {}, is {}" .format(x,y, (x/y))) 
